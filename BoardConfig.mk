ba36ae1cc28f1734e6c5600bbbe11c64c48cb64f
techyguyperplexable <154611145+techyguyperplexable@users.noreply.github.com>
2025-07-12 20:19:09 +0100

    r8s: Added missing _

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -29,7 +29,7 @@ QCOM_BT_USE_SMD_TTY := true
 TARGET_SCREEN_DENSITY := 420
 
 # Kernel
-TARGET_KERNEL_CONFIG := extreme-r8s_defconfig
+TARGET_KERNEL_CONFIG := extreme_r8s_defconfig
 
 # OTA
 TARGET_OTA_ASSERT_DEVICE := r8s
